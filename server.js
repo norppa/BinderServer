6c70622f6c0f3de1d2ea56f3053789119c01d006
Jari Haavisto <jari.haavisto@gmail.com>
2020-10-04 13:04:03 +0300

    Bare bones server

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('tiny'))
+app.use(express.json())
+
+app.use('/', require('./routers/router'))
+
+app.listen(process.env.PORT)
