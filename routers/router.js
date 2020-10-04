55567edf2c08473db19610fa1272fc6b366a0c93
Jari Haavisto <jari.haavisto@gmail.com>
2020-10-04 13:17:24 +0300

    Add Level DB

--- routers/router.js
+++ routers/router.js
@@ -1,4 +1,17 @@
 const router = require('express').Router()
+const { users } = require('../database/level')
+
+router.get('/add', (req, res) => {
+    users.put({username: 'foo', salt: 'salt', hash: 'hash'})
+    res.send('added')
+})
+router.get('/get', async (req, res) => {
+    res.send(await users.all())
+})
+router.get('/del', (req, res) => {
+    users.del('foo')
+    res.send('deleted')
+})
 
 router.get('/', (req, res) => {
     res.send('OK')
