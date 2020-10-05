67091087c91bfb97b0b96f040c6a44ef732d4aec
Jari Haavisto <jari.haavisto@gmail.com>
2020-10-05 16:24:48 +0300

    rename environment variables more uniquely to work with other servers

--- database/pool.js
+++ database/pool.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise')
 
 const dbConfig = {
     connectionLimit : 10,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.BINDER_DB_HOST,
+    port: process.env.BINDER_DB_PORT,
+    user: process.env.BINDER_DB_USERNAME,
+    password: process.env.BINDER_DB_PASSWORD,
+    database: process.env.BINDER_DB_DATABASE,
 }
 
 const pool = mysql.createPool(dbConfig)
